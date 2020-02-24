f3aef993a132d09fe291daaf8621642c8f62c8ce
Lucas Dohmen <lucas@dohmen.io>
2020-02-24 15:02:57 +0100

    Search Results

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ let { spawn } = require("child_process")
 let polka = require("polka")
 let proxy = require("http-proxy-stream")
 let port = process.env.PORT || "3000"
+let http2 = require("http2")
+let { HTTP2_HEADER_PATH, HTTP2_HEADER_STATUS } = http2.constants
+let Emitter = require("events")
+let createSearchResultParser = require("./lib/create-search-result-parser")
+
+let clientSession = http2.connect("https://www.youtube.com")
 
 // TODO: Error Handling
-// TODO: The video quality seems low, can we get it higher?
 // TODO: Do we need to strip something in the proxy function?
 polka()
 	.get("/watch", async (req, res) => {
@@ -38,6 +43,43 @@ polka()
 			`https://www.youtube.com/watch?v=${req.query.v}`
 		)
 		proxy(req, { url }, res)
+	})
+	.get("results", async (req, res) => {
+		let result = clientSession.request({
+			[HTTP2_HEADER_PATH]: `/results?search_query=${req.query.search_query}`
+		})
+
+		result.on("response", headers => {
+			res.statusCode = headers[HTTP2_HEADER_STATUS]
+
+			res.write(`<!doctype html>
+				<html lang="en">
+					<head>
+						<meta charset="utf-8">
+						<title>Search Results</title>
+					</head>
+					<body>
+						<ul>
+			`)
+
+			let emitter = new Emitter()
+			emitter.on("video", ({ title, href, meta, description }) => {
+				// href, title, description, meta
+				res.write(`<li>
+					<p><a href="${href}">${title}</a> ${description}</p>
+					<ul>
+						${meta.map(m => `<li>${m}</li>`).join("")}
+					</ul>
+				</li>`)
+			})
+			let parser = createSearchResultParser(emitter)
+
+			result.on("data", parser.parse)
+			result.on("close", () => {
+				parser.close()
+				res.end(`</ul></body></html>`)
+			})
+		})
 	})
 	.listen(port, err => {
 		if (err) throw err
